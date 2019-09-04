785de00b25feeb5a59dcc89d6674e60fe8de094f
Hien Ho <hien.ho@nextfunc.com>
2019-09-04 21:36:04 +0700

    Initial Commit

---
+++ Eight Notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Eight Notes
+//
+//  Created by mini03 on 9/4/19.
+//  Copyright © 2019 TeamLuna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
